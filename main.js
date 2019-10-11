4a385f52dca3b5e96ad429162613754ea40813bc
Simon Christmann <simon@christmann.email>
2019-10-11 04:24:38 +0200

    Add MQTT

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const unique = require('uniq');
 const $ = require('jquery');
 const PQueue = require('p-queue');
 const delay = require('delay');
+const MqttSmarthome = require('mqtt-smarthome-connect');
 
 // Unique
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
@@ -17,6 +18,12 @@ queue.add(() => delay(2000));
 queue.add(() => delay(2000));
 queue.add(() => console.log(Date.now()));
 
+const mqtt = new MqttSmarthome('ws://localhost:9001', { // works only with ws:// not with mqtt://
+    will: {topic: 'test/maintenance/online', payload: 'false', retain: true}
+});
+mqtt.on('connect', () => {
+    mqtt.publish('test/maintenance/online', true, {retain: true});
+});
 
 // Export for browser
 global.jQuery = $;
@@ -24,3 +31,5 @@ global.$ = $;
 global.unique = unique;
 global.PQueue = PQueue;
 global.queue = queue;
+global.MqttSmarthome = MqttSmarthome;
+global.mqtt = mqtt;
